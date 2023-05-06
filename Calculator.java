1ac99bf3a8d8a52afb7ff8a8e8a623453aec6a6e
minpyaythein <kaihatsu.min.pyai.tein@gmail.com>
2023-05-06 21:07:21 +0900

    basic operations but not confirmed

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,33 @@ class Calculator
     public static void main(String[] args)
     {
         Scanner calculator = new Scanner(System.in);
-        System.out.println("")
+
+        System.out.println("1番目の数字: ");
+        int a = Integer.parseInt(calculator.nextLine());
+
+        System.out.println("数学演算記号: ");
+        String x = calculator.nextLine();
+
+        System.out.println("2番目の数字: ");
+        int b = Integer.parseInt(calculator.nextLine());
+
+        if (x == "+")
+        {
+            System.out.println(a + " + " + b + " = " + (a + b));
+        }
+        else if (x == "-")
+        {
+            System.out.println(a + " - " + b + " = " + (a - b));
+        }
+        else if (x == "*")
+        {
+            System.out.println(a + " * " + b + " = " + (a * b));
+        }
+        else if (x == "/")
+        {
+            System.out.println(a + " / " + b + " = " + (a / b));
+        }
+        
+        calculator.close();
     }
 }
